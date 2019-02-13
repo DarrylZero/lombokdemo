6f62336d7fb8c4b309e4e087e6b44bd22162311b
Vladimir.Bogodukhov <vladimir.bogodukhov@billing.ru>
2019-02-13 18:09:06 +0300

    data

--- simpleprocessorchecks/src/main/java/com/steammachine/data/TDProbe.java
+++ simpleprocessorchecks/src/main/java/com/steammachine/data/TDProbe.java
@@ -3,7 +3,7 @@ package com.steammachine.data;
 
 import com.staemmachine.annotationprocessors.techdebt.annotations.TechnicalDebt;
 
-@TechnicalDebt(value = "11.02.2019")
+@TechnicalDebt(value = "20.02.2019")
 //@TechnicalDebt(value = "asdasd")
 public class TDProbe {
 
--- simpleprocessorchecks/src/main/java/com/steammachine/data/TDProbe2.java
+++ simpleprocessorchecks/src/main/java/com/steammachine/data/TDProbe2.java
@@ -1,12 +1,46 @@
 package com.steammachine.data;
 
+import com.staemmachine.annotationprocessors.spicollector.InterfaceToSupport;
+import com.staemmachine.annotationprocessors.spicollector.annotionons.Collected;
 import com.staemmachine.annotationprocessors.techdebt.annotations.TechnicalDebt;
 
-@TechnicalDebt(value = "01.02.2020")
-//@TechnicalDebt(value = "asdasd")
+@TechnicalDebt(value = "21.02.2022")
 public class TDProbe2 {
 
 
+    private static class Data implements InterfaceToSupport {
+
+        @Override
+        public void methodToCall() {
+
+        }
+    }
+
+
+    @Collected
+    public static InterfaceToSupport data3() {
+        return new Data();
+    }
+
+    @Collected
+    public static InterfaceToSupport data() {
+        return new InterfaceToSupport() {
+            @Override
+            public void methodToCall() {
+
+            }
+        };
+    }
+
+    @Collected
+    public static InterfaceToSupport data2() {
+        return new Data();
+    }
+
+    @Collected
+    public void data4() {
+
+    }
 
 
 }
