4ecb819e000d37624279f97a0afca080d1acab74
DarrylZero <ubob@inbox.ru>
2019-02-14 20:09:18 +0300

    s

--- simpleprocessorchecks/src/main/java/TDProbe.java
+++ simpleprocessorchecks/src/main/java/TDProbe.java
@@ -1,15 +1,14 @@
-package com.steammachine.data;
-
-
+import com.staemmachine.annotationprocessors.spicollector.InterfaceToSupport;
 import com.staemmachine.annotationprocessors.spicollector.annotionons.Collected;
 import com.staemmachine.annotationprocessors.techdebt.annotations.TechnicalDebt;
 
 @TechnicalDebt(value = "22.02.2019")
 @Collected
 //@TechnicalDebt(value = "asdasd")
-public class TDProbe {
+public class TDProbe {//implements InterfaceToSupport {
 
+ //   @Override//
+    public void methodToCall() {
 
-
-
+    }
 }
--- simpleprocessorchecks/src/main/java/com/steammachine/data/TDProbe2.java
+++ simpleprocessorchecks/src/main/java/com/steammachine/data/TDProbe2.java
@@ -1,21 +1,14 @@
 package com.steammachine.data;
 
-<<<<<<< HEAD
-import com.staemmachine.annotationprocessors.spicollector.annotionons.Collected;
-import com.staemmachine.annotationprocessors.techdebt.annotations.TechnicalDebt;
-
-@TechnicalDebt(value = "01.02.2020")
-@Collected
-public class TDProbe2 {
-=======
 import com.staemmachine.annotationprocessors.spicollector.InterfaceToSupport;
 import com.staemmachine.annotationprocessors.spicollector.annotionons.Collected;
 import com.staemmachine.annotationprocessors.techdebt.annotations.TechnicalDebt;
 
+@Collected
 @TechnicalDebt(value = "21.02.2022")
 public class TDProbe2 {
 
-
+//    @MetaInfServices
     private static class Data implements InterfaceToSupport {
 
         @Override
@@ -50,6 +43,4 @@ public class TDProbe2 {
 
     }
 
-
->>>>>>> 6f62336d7fb8c4b309e4e087e6b44bd22162311b
 }
